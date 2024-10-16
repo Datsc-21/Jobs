7308479fcdbe81f48354d4f337642996c2df3fbd
Danny Scholze <dannyscholze21@gmail.com>
2024-10-16 13:48:35 -0400

    Alteração no codigo

--- teste2.c
+++ teste2.c
@@ -3,17 +3,27 @@
 
 int main(){
 
+struct elemento{ 
 char nome[30];
 char cep[30];
+int a,b;
+float preco;
+int conta;
+} Elem;
 
 printf("Digite aqui o seu nome: ");
-scanf("%s", nome);
+scanf("%s", Elem.nome);
 printf("Digite aqui o seu cep: ");
-scanf("%s", cep);
+scanf("%s", Elem.cep);
+printf("Digite os valores: ");
+scanf("%d %d", &Elem.a,&Elem.b);
+
+Elem.conta = Elem.a + Elem.b;
 
 system("pause");
 
-printf("Seu Nome: %s\n Seu Cep: %s ", nome, cep);
+printf("Seu Nome: %s\n Seu Cep: %s\n ", Elem.nome, Elem.cep);
+printf("A conta deu: %d", Elem.conta);
 
 return 0;
 }
